a24cbb089cd17bbff8a785aaccd8116ec38039d7
Martin Bøhm Jensen <mart020d@elev.techcollege.dk>
2019-01-16 12:45:44 +0100

    reset button

--- content/js/reset.js
+++ content/js/reset.js
@@ -1,10 +1,11 @@
 // reset function, resets the game after being activated
 function reset() {
-  document.getElementById("scoreboard1").innerHTML = "0";
-  document.getElementById("scoreboard2").innerHTML = "0";
-  num1 = 0;
-  num2 = 0;
-  ball.style.top = startTopBall+"vh"
-  ball.style.left = startLeftBall+"vw"
-countTime = -1;
+//   document.getElementById("scoreboard1").innerHTML = "0";
+//   document.getElementById("scoreboard2").innerHTML = "0";
+//   num1 = 0;
+//   num2 = 0;
+//   ball.style.top = startTopBall+"vh"
+//   ball.style.left = startLeftBall+"vw"
+// countTime = -1;
+window.location.reload()
 };
