f0946d6c6330218a51b69c22a1935696c4f17494
Zainde <42198931+Zainde@users.noreply.github.com>
2019-01-24 13:39:21 +0100

    reset js kommentarer

--- content/js/reset.js
+++ content/js/reset.js
@@ -1,13 +1,14 @@
 // reset function, resets the game after being activated
 function reset() {
   // martin trying to write something which could work \/
-//   document.getElementById("scoreboard1").innerHTML = "0";
-//   document.getElementById("scoreboard2").innerHTML = "0";
-//   num1 = 0;
-//   num2 = 0;
-//   ball.style.top = startTopBall+"vh"
-//   ball.style.left = startLeftBall+"vw"
-
+  /*
+  document.getElementById("scoreboard1").innerHTML = "0";
+  document.getElementById("scoreboard2").innerHTML = "0";
+  num1 = 0;
+  num2 = 0;
+  ball.style.top = startTopBall+"vh"
+  ball.style.left = startLeftBall+"vw"
+*/
 // martin ending up taking the easy solution \/
 window.location.reload()
 };
